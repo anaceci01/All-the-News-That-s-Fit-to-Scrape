9ddad8b869b51e7e1936d3df26883e80617adf27
Ana Davis <anac.davis@gmail.com>
2020-01-22 20:16:39 -0700

    there was an error on the notesjs

--- config/route.js
+++ config/route.js
@@ -1,3 +1,8 @@
+var scrape = require("../scripts/scrape");
+
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
 module.exports = function(router) {
 
     router.get("/", function(req, res) {
@@ -7,5 +12,17 @@ module.exports = function(router) {
     router.get("/saved", function(req, res) {
         res.render("saved");
     });
-
+    router.get("/api/fetch", function(req, res) {
+        headlinesController.fetch(function(err, docs) {
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "No new articles today. Check back tomorrow!"
+                });
+            } else {
+                res.json({
+                    message: "Added" + docs.insertedCount + " new articles!"
+                });
+            }
+        });
+    });
 }
