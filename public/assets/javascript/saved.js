a9bae5fca9285342b529742394ad41038aba580e
Ana Davis <anac.davis@gmail.com>
2020-01-23 11:58:37 -0700

    grab notes attached to the id and display them. function to create modal and close it

--- public/assets/javascript/saved.js
+++ public/assets/javascript/saved.js
@@ -1,13 +1,11 @@
-import { inherits } from "util";
-
 $(document).ready(function() {
     var articleContainer = $(".article-container");
 
     //adding event listeners
-    $(document).on("click", "btn-delete", handleArticleDelete);
-    $(document).on("click", "btn.notes", handleArticleNotes);
-    $(document).on("click", "btn.save", handleNoteSave);
-    $(document).on("click", "btn.note-delete", handleNoteDelete);
+    $(document).on("click", ".btn-delete", handleArticleDelete);
+    $(document).on("click", ".btn.notes", handleArticleNotes);
+    $(document).on("click", ".btn.save", handleNoteSave);
+    $(document).on("click", ".btn.note-delete", handleNoteDelete);
 
     initPage();
 
@@ -51,7 +49,75 @@ $(document).ready(function() {
         }
 
         function renderEmpty() {
+            var emptyAlert =
+                $(["<div class='alert alert-warning text-center'>",
+                    "<h4> Sorry, we do not have any new articles.</h4>",
+                    "</div>",
+                    "<div class='panel panel-default'>",
+                    "<div class='panel-heading text-center'>",
+                    "<h3>Would you like to brouse available artices?'</h3>",
+                    "</div>",
+                    "<div class='panel-body text-center'>",
+                    "<h4><a href='/'>Browse Articles</a></h4>",
+                    "</div>",
+                    "</div>"
+                ].join(""));
+            // append data to the page
+            articleContainer.append(emptyAlert);
+        }
 
+        function renderNoteList(data) {
+            var notesToRender = [];
+            var currentNote;
+            if (!data.notes.length) {
+                currentNote = [
+                    "<li class='list-group-item'>",
+                    "No notes for this article yet",
+                    "</li>"
+                ].join("");
+                notesToRender.push(currentNote);
+            } else {
+                for (var i = 0; i < data.notes.length; i++) {
+                    currentNote = $([
+                        "<li class='listgroup-item note" > ,
+                        data.notes[i].noteText,
+                        "<button class='btn btn-danger note-delete'>x</button",
+                        "</li>"
+                    ].join(""));
+                    currentNote.children("button").data("_id", data.notes[i]._id);
+                    notesToRender.push(currentNote);
+                }
+            }
+            $(".note-container").append(notesToRender);
+        }
+
+        function handleArticleDelete() {
+            var articleToDelete = $(this).parents(".panel").data();
+            $.ajax({
+                method: "DELETE",
+                url: "api/headlines/" + articleToDelete._id
+            }).then(function(data) {
+                if (data.ok) {
+                    initPage();
+                }
+            });
+        }
+
+        function handleArticleNotes() {
+            var currentArticle = $(this).parents(".panel").data();
+            $.get("/api/notes/" + currentArticle._id).then(function(data) {
+                var modalText = [
+                    "<div class ='container-fluid text-center'>",
+                    "<h4>Notes For Article: ",
+                    currentArticle._id,
+                    "</h4>",
+                    "<hr />",
+                    "<ul class='list-group note-container'>",
+                    "</ul>",
+                    "<textarea placeholder='New Note' rows='4' cols='60'></textarea>",
+                    "<button class='btn btn-success save'>Save Note</button>",
+                ]
+            })
         }
     }
 })
