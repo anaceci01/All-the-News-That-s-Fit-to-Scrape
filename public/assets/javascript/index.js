74c9b366ea9e44d41615e3635aa00528ab5aa63f
Ana Davis <anac.davis@gmail.com>
2020-01-22 22:43:56 -0700

    ajax and saving articles. created init function on prev commit

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -42,11 +42,35 @@ $(doucument).ready(function() {
                 "</div>",
                 "</div>"
             ].join(""));
+        panel.data("_id", article._id);
+        return panel;
+    }
+
+    function renderEmpty() {
+
         articleContainer.append(emptyAlert);
     }
 
     function handleArticleSave() {
         var articleToSave = $(this).parents(".panel").data();
         articleToSave.saved = true;
+        $.ajax({
+                method: "PATCH",
+                url: "/api/headlines",
+                data: articleToSave
+            })
+            .then(function(data) {
+                if (data.ok) {
+                    initPage();
+                }
+            });
+    }
+
+    function handleArticleScrape() {
+        $.get("/api/fetch")
+            .then(function(data) {
+                initPage();
+                bootbox.alert("<h3 class='text-center m-top-80'>" + data.message + "<h3>");
+            })
     }
 })
