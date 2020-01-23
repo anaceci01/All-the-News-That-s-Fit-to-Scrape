44cd7803b5fc91e602d87172c07d5fbba46aa892
Ana Davis <anac.davis@gmail.com>
2020-01-22 22:25:54 -0700

    render and scrape articles

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -23,5 +23,30 @@ $(doucument).ready(function() {
         for (var i = 0; i < articles.length; i++) {
             articlePanels.push(createPanel(articles[i]));
         }
+        articleContainer.append(articlePanels);
+    }
+
+    function createPanel(article) {
+        var panel =
+            $(["<div class = 'panel panel-default'>",
+                "<div class = 'panel-heading'>",
+                "<h3>",
+                article.headline,
+                "<a class = 'btn btn-success save'>",
+                "Save Article",
+                "</a>",
+                "</h3",
+                "</div",
+                "<div class='panel-body'>",
+                article.summary,
+                "</div>",
+                "</div>"
+            ].join(""));
+        articleContainer.append(emptyAlert);
+    }
+
+    function handleArticleSave() {
+        var articleToSave = $(this).parents(".panel").data();
+        articleToSave.saved = true;
     }
 })
